977097248684f138994d7afbc1c5c0a7371f9ff5
bhavesh_tiwari <btiwari441@gmail.com>
2019-05-12 18:13:38 +0530

    Updates

---
+++ precache-manifest.3c3890f5cf0296352e7f0ed0aa9aadf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b94e270390cce35d61",
+    "url": "/reactappytclone/static/js/runtime~main.a77f9373.js"
+  },
+  {
+    "revision": "d866cf6f288d50ac2f27",
+    "url": "/reactappytclone/static/js/main.4777adab.chunk.js"
+  },
+  {
+    "revision": "0e21496b543ac96ea7f5",
+    "url": "/reactappytclone/static/js/2.82c7acfd.chunk.js"
+  },
+  {
+    "revision": "d866cf6f288d50ac2f27",
+    "url": "/reactappytclone/static/css/main.7f8ec2fa.chunk.css"
+  },
+  {
+    "revision": "bfd4dabcc764a343b9e2c56f6b4f320c",
+    "url": "/reactappytclone/index.html"
+  }
+];
